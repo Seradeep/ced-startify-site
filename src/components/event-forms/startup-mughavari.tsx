4d6d012b219d98562994f9df2dac9267e477567e
Omprakash  D <omugan07@gmail.com>
2024-11-20 11:44:14 +0530

    feat: update payment amounts for Startup Cafe and Startup Mughavari applications

--- src/components/event-forms/startup-mughavari.tsx
+++ src/components/event-forms/startup-mughavari.tsx
@@ -104,7 +104,7 @@ export default function StartupMughavariForm({
     <div className="max-w-2xl mx-auto p-4 sm:p-6">
       <h1 className="text-3xl font-bold mb-2">Startup Mughavari Application</h1>
       <TypographyP className="!mt-0 mb-4">
-        You need to pay Rs.1180/-(including taxes) at the time of submission of
+        You need to pay Rs.1250/-(including all taxes) at the time of submission of
         your applications
       </TypographyP>
       <FormStepper currentStep={step} totalSteps={totalSteps} />
@@ -203,7 +203,7 @@ export default function StartupMughavariForm({
           onOpen={onPaymentBtnOpen}
           callbackFn={handleSubmit}
           event={{
-            amount: "1180",
+            amount: "1250",
             name: "Startup Mughavari Application",
           }}
         />
